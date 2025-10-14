26bf6f46187392359e78605b876b80b765e84a1d
Leopold Johannes Meinel <leo@meinel.dev>
2025-10-14 22:56:21 +0200

    Init: Add eduroam-cli.sh and init

---
+++ README.md
@@ -0,0 +1,45 @@
+# eduroam-helper
+
+This is a collection of scripts to configure eduroam with a pkcs12 certificate bundle.
+
+The certificate bundle should have been provided by your university.
+
+## Basic usage
+
+### `I` GUI
+
+:information_source: | Please note that this might be outdated. For (sadly somewhat overcomplicated but) up-to-date instructions, check [this](https://doku.tid.dfn.de/de:eduroam:easyroam#installation_der_easyroam_profile_auf_linux_geraeten).
+
+1. Log in to https://www.easyroam.de
+2. Click on `Manual installation > Linux` and give a profile name to download `example.p12`. The file name might be different.
+
+### `II` CLI
+
+After you have downloaded `example.p12` (`I`), open your terminal.
+
+```sh
+# Navigate to the directory where 'example.p12' is stored
+cd ~/Downloads
+
+# Clone this repository and make eduroam-nmcli.sh executable
+git clone https://codeberg.org/ce-it-knowledge-exchange/eduroam-helper.git
+chmod +x ~/Downloads/eduroam-helper/eduroam-nmcli.sh
+
+# Execute eduroam-nmcli.sh
+# If you are using doas, execute only the next line:
+#     > doas ~/Downloads/eduroam-helper/eduroam-nmcli.sh ~/Downloads/example.p12
+sudo ~/Downloads/eduroam-helper/eduroam-nmcli.sh ~/Downloads/example.p12
+```
+
+## Help
+
+### eduroam-nmcli.sh
+
+```
+Usage: eduroam-nmcli.sh [pkcs12 certificate bundle]
+
+Configure eduroam with nmcli and a pkcs12 certificate bundle
+
+Parameters:
+    [pkcs12 certificate bundle]  Path to certificate bundle, fex. '~/Downloads/example.p12'
+```
---
+++ eduroam-ncmli.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+###
+# File: eduroam-ncmli.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2025 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
+# This script is heavily inspired by: https://git.uni-greifswald.de/URZ-Public/easyroam/src/branch/main/configure-eduroam-with-easyroam
+
+# Fail on error
+set -e
+
+# Define functions
+log_err() {
+    /usr/bin/logger -s -p local0.err <<<"$(basename "${0}"): ${*}"
+}
+log_warning() {
+    /usr/bin/logger -s -p local0.warning <<<"$(basename "${0}"): ${*}"
+}
+cert_bundle_err_exit() {
+    log_err "Certificate bundle '${1}' is invalid or could not be processed."
+    exit 1
+}
+print_help() {
+    echo "Usage: $(basename "${0}") [pkcs12 certificate bundle]"
+    echo ""
+    echo "Configure eduroam with nmcli and a pkcs12 certificate bundle"
+    echo ""
+    echo "Parameters:"
+    echo "    [pkcs12 certificate bundle]  Path to certificate bundle, fex. '~/Downloads/example.p12'"
+}
+
+# Check ${EUID} and parameter
+if [[ "${EUID}" -ne 0 ]]; then
+    log_err "You can only run this script if you are root."
+    exit 1
+fi
+if [[ -f "${1}" ]]; then
+    log_err "Please specify a valid pkcs12 certificate bundle."
+    print_help
+    exit 1
+fi
+
+# Check if network configuration is valid
+if [[ -z "$(iw dev)" ]]; then
+    log_err "No WiFi device detected."
+    exit 1
+fi
+if [[ -z "$(which nmcli)" ]]; then
+    log_err "No 'nmcli' command found."
+    exit 1
+fi
+
+# Check if we are using openssl >=3
+OPENSSL_OPTIONS=""
+[[ "$(openssl -v | awk '{print $2}' | cut -d '.' -f1)" -ge 3 ]] &&
+    OPENSSL_OPTIONS="-legacy"
+
+# Generate certificates
+## Create ${CERT_DIR}
+CERT_DIR=/etc/eduroam-certs
+mkdir -p "${CERT_DIR}"
+## Generate client_certs and ca_certs
+CERT_BUNDLE="${1}"
+openssl pkcs12 -in "${CERT_BUNDLE}" ${OPENSSL_OPTIONS} -passin pass: -nokeys -clcerts -out "${CERT_DIR}"/client_certs.pem ||
+    cert_bundle_err_exit "${CERT_BUNDLE}"
+openssl pkcs12 -in "${CERT_BUNDLE}" ${OPENSSL_OPTIONS} -passin pass: -nokeys -cacerts -out "${CERT_DIR}"/ca_certs.pem ||
+    cert_bundle_err_exit "${CERT_BUNDLE}"
+## Generate client_key encrypted with ${PASSPHRASE}
+PASSPHRASE="$(head -c 1280 /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 128)"
+openssl pkcs12 -in "${CERT_BUNDLE}" ${OPENSSL_OPTIONS} -passin pass: -passout pass:"${PASSPHRASE}" -nocerts -out "${CERT_DIR}"/client_key.pem ||
+    cert_bundle_err_exit "${CERT_BUNDLE}"
+
+# Add nmcli connection for ${WIFI_NAME}
+WIFI_NAME="eduroam"
+IDENTITY="$(openssl x509 -noout -in "${CERT_DIR}"/client_certs.pem -subject | awk '{print $1}' | sed 's/.*=//' | sed 's/,//' | tr -d "[:space:]")"
+nmcli connection add type wifi con-name "${WIFI_NAME}" ssid "${WIFI_NAME}" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap tls 802-1x.identity "${IDENTITY}" 802-1x.ca-cert "${CERT_DIR}"/ca_certs.pem 802-1x.client-cert "${CERT_DIR}"/client_certs.pem 802-1x.private-key-password "${PASSPHRASE}" 802-1x.private-key "${CERT_DIR}"/client_key.pem
+
+# Notify user if script has finished successfully
+echo "'$(basename "${0}")' has finished successfully."

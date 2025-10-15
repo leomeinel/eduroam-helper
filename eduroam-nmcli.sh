d4ce356439256aa07da083d70afe60855c316fda
Leopold Johannes Meinel <leo@meinel.dev>
2025-10-15 11:07:57 +0200

    Fix: Wrong IDENTITY being used
    Not sure why the original script did what it did..

--- eduroam-nmcli.sh
+++ eduroam-nmcli.sh
@@ -82,7 +82,9 @@ chmod 700 "${CERT_DIR}"
 chmod 0400 "${CERT_DIR}"/*
 
 # Add nmcli connection for ${CONNECTION_NAME}
-IDENTITY="$(openssl x509 -noout -in "${CERT_DIR}"/client_certs.pem -subject | awk '{print $1}' | sed 's/.*=//' | sed 's/,//' | tr -d "[:space:]")"
+IDENTITY="$(openssl x509 -noout -in "${CERT_DIR}"/client_certs.pem -subject)"
+IDENTITY="${IDENTITY#*CN=}"
+IDENTITY="${IDENTITY%%,*}"
 nmcli connection add type wifi con-name "${CONNECTION_NAME}" ssid "${CONNECTION_NAME}" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap tls 802-1x.identity "${IDENTITY}" 802-1x.ca-cert "${CERT_DIR}"/ca_certs.pem 802-1x.client-cert "${CERT_DIR}"/client_certs.pem 802-1x.private-key-password "${PASSPHRASE}" 802-1x.private-key "${CERT_DIR}"/client_key.pem
 
 # Notify user if script has finished successfully
